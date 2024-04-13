a37a472fd83ec13fc4e5f066c009d40591b3a41d
Brian <brian.cabrera@tufud.co>
2024-04-13 18:46:19 -0300

    [CONTROLLER] created StatisticsController

---
+++ src/main/java/com/statistics/api/controller/StatisticsController.java
@@ -0,0 +1,40 @@
+package com.statistics.api.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.statistics.api.service.StatisticsService;
+import com.statistics.api.utils.UtilsService;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/statistics")
+public class StatisticsController {
+
+    private final StatisticsService statisticsService;
+    private final UtilsService utilsService;
+
+    public StatisticsController(StatisticsService statisticsService, UtilsService utilsService) {
+        super();
+        this.statisticsService = statisticsService;
+        this.utilsService = utilsService;
+    }
+
+    @GetMapping("/{short_url}") 
+    public ResponseEntity<Void> getShortUrlStatistics(HttpServletRequest request,
+        @PathVariable(value = "short_url") String shortURL) throws Exception {
+
+        return utilsService.redirect(null);
+    }
+
+    @GetMapping("/long_url") 
+    public ResponseEntity<Void> getLongURLStatistics(HttpServletRequest request,
+        @PathVariable(value = "long_url") String shortURL) throws Exception {
+
+        return utilsService.redirect(null);
+    }
+    
+}
