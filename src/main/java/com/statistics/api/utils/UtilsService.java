c6dfefeb0bf17524bc84d420ded41c65da6a00c1
Brian <brian.cabrera@tufud.co>
2024-04-14 03:51:47 -0300

    [UTILS] added methods in UtilsService for object mapping and time formatting

--- src/main/java/com/statistics/api/utils/UtilsService.java
+++ src/main/java/com/statistics/api/utils/UtilsService.java
@@ -1,17 +1,25 @@
 package com.statistics.api.utils;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.stream.Collectors;
 
+import org.json.JSONObject;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 import com.statistics.api.domain.LongURLStatistics;
 import com.statistics.api.domain.ShortURLStatistics;
-import com.statistics.api.domain.URLDeviceInformation;
+import com.statistics.api.domain.ClientDeviceInformation;
+import com.statistics.api.domain.ClientLocationInformation;
 import com.statistics.api.dto.LongURLStatisticsDto;
 import com.statistics.api.dto.ShortURLStatisticsDto;
-import com.statistics.api.dto.URLDeviceInformationDto;
+import com.statistics.api.request.SqsURLAccessedEventRequest;
+import com.statistics.api.request.URLPairRequest;
+import com.statistics.api.dto.ClientDeviceInformationDto;
+import com.statistics.api.dto.ClientLocationInformationDto;
 
 @Service
 public class UtilsService {
@@ -44,17 +52,57 @@ public class UtilsService {
     public ShortURLStatisticsDto convertShortURLStatisticsToDto(ShortURLStatistics shortURL) {
         ShortURLStatisticsDto dto = modelMapper.map(shortURL, ShortURLStatisticsDto.class);
 
-        if (shortURL.getDevices() != null && !shortURL.getDevices().isEmpty()) {
-            dto.setDevices(shortURL.getDevices().stream().map(device -> {
-                return convertURLDeviceInformationToDto(device);
+        if (shortURL.getConnections() != null && !shortURL.getConnections().isEmpty()) {
+            dto.setConnections(shortURL.getConnections().stream().map(device -> {
+                return convertURLLocationInformationToDto(device);
             }).collect(Collectors.toList()));
         }
 
         return dto;
     }
 
-    private URLDeviceInformationDto convertURLDeviceInformationToDto(URLDeviceInformation URLDeviceInformation) {
-        return modelMapper.map(URLDeviceInformation, URLDeviceInformationDto.class);
+    private ClientLocationInformationDto convertURLLocationInformationToDto(ClientLocationInformation URLLocationInformation) {
+        ClientLocationInformationDto dto = modelMapper.map(URLLocationInformation, ClientLocationInformationDto.class);
+
+        dto.setClientDeviceInformation(convertClientDeviceInformationToDto(URLLocationInformation.getClientDeviceInformation()));
+
+        return dto;
+    }
+
+    private ClientDeviceInformationDto convertClientDeviceInformationToDto(ClientDeviceInformation URLDeviceInformation) {
+        return modelMapper.map(URLDeviceInformation, ClientDeviceInformationDto.class);
+    }
+
+    public ClientLocationInformation createClientLocationInformationFromDto(ClientLocationInformationDto URLLocationInformation) {
+        return modelMapper.map(URLLocationInformation, ClientLocationInformation.class);
+    }
+
+    public LocalDateTime convertMillisToLocalDateTime(String millis) {
+        long timestamp = Long.parseLong(millis);
+
+        Instant instant = Instant.ofEpochMilli(timestamp);
+
+        return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+    }
+
+    public URLPairRequest convertJSONObjectToSqsURLEventRequest(JSONObject body) {
+        URLPairRequest request = new URLPairRequest();
+
+        request.setLongURL(body.getString("long_url") != null ? body.getString("long_url") : null);
+        request.setShortURL(body.getString("short_url") != null ? body.getString("short_url") : null);
+
+        return request;
+    }
+
+    public SqsURLAccessedEventRequest convertJSONObjectToSqsURLAccessedEventRequest(JSONObject body) {
+        SqsURLAccessedEventRequest request = new SqsURLAccessedEventRequest();
+
+        request.setShortURL(body.getString("short_url") != null ? body.getString("short_url") : null);
+        request.setClientIP(body.getString("client_ip") != null ? body.getString("client_ip") : null);
+        request.setUserAgent(body.getString("user_agent") != null ? body.getString("user_agent") : null);
+        request.setAccessedAt(body.getString("accessed_at") != null ? body.getString("accessed_at") : null);
+
+        return request;
     }
 
 }
