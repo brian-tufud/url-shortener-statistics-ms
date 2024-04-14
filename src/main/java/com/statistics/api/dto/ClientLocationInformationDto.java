0ab3a97e63dfdd2c5e76c67e3460a1c655a48e80
Brian <brian.cabrera@tufud.co>
2024-04-14 03:45:55 -0300

    [DTO] created ClientLocationInformationDto

---
+++ src/main/java/com/statistics/api/dto/ClientLocationInformationDto.java
@@ -0,0 +1,49 @@
+package com.statistics.api.dto;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ClientLocationInformationDto {
+
+    private Long id;
+
+    @JsonProperty("continent_code")
+    private String continentCode;
+
+    @JsonProperty("continent_name")
+    private String continentName;
+
+    @JsonProperty("country_code")
+    private String countryCode;
+
+    @JsonProperty("country_name")
+    private String countryName;
+
+    @JsonProperty("region_code")
+    private String regionCode;
+
+    @JsonProperty("region_name")
+    private String regionName;
+
+    @JsonProperty("city")
+    private String cityName;
+
+    @JsonProperty("zip")
+    private String zipCode;
+
+    @JsonProperty("accessed_at")
+    private LocalDateTime accessedAt;
+
+    @JsonProperty("client_device_information")
+    private ClientDeviceInformationDto clientDeviceInformation;
+}
