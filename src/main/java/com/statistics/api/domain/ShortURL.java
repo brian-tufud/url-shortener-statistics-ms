b77ce4c52e3f1214abb01c30bf043fd915db07dc
Brian <brian.cabrera@tufud.co>
2024-04-13 22:28:56 -0300

    [DOMAIN] created ShortURL entity

---
+++ src/main/java/com/statistics/api/domain/ShortURL.java
@@ -0,0 +1,37 @@
+package com.statistics.api.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "short_url")
+public class ShortURL {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+    @Column(name = "short_url")
+    private String shortURL;
+
+    @Column(name = "last_accessed")
+    private LocalDateTime lastAccessed;
+
+    @Column(name = "times_used")
+    private Integer timesUsed;
+
+    @ManyToOne
+    @JoinColumn(name = "long_url")
+    private LongURL longURL;
+
+	@OneToMany(mappedBy = "shortURL", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<URLDeviceInformation> devices = new ArrayList<>();
+}
