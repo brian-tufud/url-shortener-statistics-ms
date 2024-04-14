99de059ae8765085066b9786f8af944ec0e133a5
Brian <brian.cabrera@tufud.co>
2024-04-13 22:29:06 -0300

    [DOMAIN] created URLDeviceInformation entity

---
+++ src/main/java/com/statistics/api/domain/URLDeviceInformation.java
@@ -0,0 +1,43 @@
+package com.statistics.api.domain;
+
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "url_device_information")
+public class URLDeviceInformation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "device_class")
+    private String deviceClass;
+
+    @Column(name = "device_name")
+    private String deviceName;
+
+    @Column(name = "device_brand")
+    private String deviceBrand;
+
+    @Column(name = "os_name")
+    private String osName;
+
+    @Column(name = "os_version")
+    private String osVersion;
+
+    @Column(name = "agent_class")
+    private String agentClass;
+
+    @Column(name = "agent_version")
+    private String agentVersion;
+
+    @ManyToOne
+    @JoinColumn(name = "short_url")
+    private ShortURL shortURL;
+    
+}
