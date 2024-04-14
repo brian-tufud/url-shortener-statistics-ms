d464767089530680d51befdf223d6457b0d7d53b
Brian <brian.cabrera@tufud.co>
2024-04-14 03:53:17 -0300

    [SERVICE] created EventService for sqs message processing

---
+++ src/main/java/com/statistics/api/service/EventService.java
@@ -0,0 +1,39 @@
+package com.statistics.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.statistics.api.request.SqsURLAccessedEventRequest;
+import com.statistics.api.utils.EventTypes;
+import com.statistics.api.utils.UtilsService;
+
+import org.json.JSONObject;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private UtilsService utilsService;
+
+    @Autowired
+    private ShortURLStatisticsService shortURLStatisticsService;
+
+    public void processEvent(JSONObject event) {
+        String eventType = event.getString("event");
+
+        switch (eventType) {
+            case EventTypes.URL_ACCESSED:
+                processURLAccessedEvent(event);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid event type");
+        }
+    }
+
+    private void processURLAccessedEvent(JSONObject event) {
+        SqsURLAccessedEventRequest body = utilsService.convertJSONObjectToSqsURLAccessedEventRequest(event);
+
+        shortURLStatisticsService.updateShortURLStatistics(body);
+    }
+
+}
